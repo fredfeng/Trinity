67df583ea68c0efbd76bb7fe6a1eceea404baac5
Yu Feng <fengyu8299@gmail.com>
2019-05-21 17:50:37 -0700

    buggy version

--- tyrell/enumerator/bidirection_smt.py
+++ tyrell/enumerator/bidirection_smt.py
@@ -1,6 +1,8 @@
 from z3 import *
 from collections import deque
 from .enumerator import Enumerator
+from functools import reduce
+
 
 from .. import dsl as D
 from ..logger import get_logger
@@ -20,8 +22,19 @@ class ASTNode:
         self.children = children
         self.production = None
 
+# Each statement has opcode, args, and lhs.
+class Stmt:
+    def __init__(self, opcode, args, lhs):
+        self.opcode = opcode
+        self.args = args
+        self.lhs = lhs
+        #FIXME: assuming all statements return tables.
+        self.type = 'Table'
+    
+    def __repr__(self):
+        return str(self.lhs) + ' = ' + str(self.opcode) + '(' + str(self.args) + ')'
 
-# FIXME: Currently this enumerator requires an "Empty" production to function properly
+
 class BidirectEnumerator(Enumerator):
     # z3 solver
     z3_solver = Solver()
@@ -38,100 +51,51 @@ class BidirectEnumerator(Enumerator):
     # map from internal k-tree to nodes of program
     program2tree = {}
 
-    def initLeafProductions(self):
-        for p in self.spec.productions():
-            # FIXME: improve empty integration
-            if not p.is_function() or str(p).find('Empty') != -1:
-                self.leaf_productions.append(p)
+    def createStmtConstraints(self):
+        functions = list(filter(lambda x: x.is_function() and x.id > 0, self.spec.productions()))
+        for p in functions:
+            print(p, p.id)
 
-    def createVariables(self, solver):
-        for x in range(0, len(self.nodes)):
-            name = 'n' + str(x + 1)
-            v = Int(name)
-            self.variables.append(v)
-            # variable range constraints
-            solver.add(And(v >= 0, v < self.spec.num_productions()))
-            hname = 'h' + str(x + 1)
-            h = Int(hname)
-            self.variables_fun.append(h)
-            # high variables range constraints
-            solver.add(And(h >= 0, h <= 1))
+        # for st in self.lines:
+        for i_loc in range(0, self.loc):
+            st = self.lines[i_loc]
+            # Opcode has to be one of the high-order functions
+            opcode = st.opcode
+            ctr_opcode = reduce(lambda a,b: Or(a, b.id == opcode), functions, False)
+            self.z3_solver.add(ctr_opcode)
 
-    def createOutputConstraints(self, solver):
-        '''The output production matches the output type'''
-        ctr = None
-        for p in self.spec.get_productions_with_lhs(self.spec.output):
-            if ctr is None:
-                # variables[0] is the root of the tree
-                ctr = self.variables[0] == p.id
-            else:
-                ctr = Or(ctr, self.variables[0] == p.id)
-        solver.add(ctr)
+            # All vars defined beforehand.
+            def_vars = list(map(lambda x: x.lhs, self.lines[:i_loc])
 
-    def createLocConstraints(self, solver):
-        '''Exactly k functions are used in the program'''
-        ctr = self.variables_fun[0]
-        for x in range(1, len(self.variables_fun)):
-            ctr += self.variables_fun[x]
-        ctr_fun = ctr == self.loc
-        solver.add(ctr_fun)
+            # Each opcode will enforce constraints to its children
+            for i in range(0, self.max_children):
+                # print('line: ', opcode, ' arg: ', i)
+                arg = st.args[i]
+                for p in functions:
+                    if i < len(p.rhs):
+                        child_type = str(p.rhs[i])
+                        child_prods = self.spec.get_productions_with_lhs(child_type)
+                        child_prods = list(map(lambda y: y.id (filter(lambda x: (not x.is_function()), child_prods))))
+                        if child_type == st.type:
+                            child_prods = child_prods + def_vars
+                        ctr_arg = reduce(lambda a,b: Or(a, b == arg), child_prods, False)
+                        self.z3_solver.add(Implies(opcode == p.id, ctr_arg))
+                    else:
+                        self.z3_solver.add(arg == -1)
 
-    def createInputConstraints(self, solver):
-        '''Each input will appear at least once in the program'''
-        input_productions = self.spec.get_param_productions()
-        for x in range(0, len(input_productions)):
-            ctr = None
-            for y in range(0, len(self.nodes)):
-                if ctr is None:
-                    ctr = self.variables[y] == input_productions[x].id
-                else:
-                    ctr = Or(self.variables[y] == input_productions[x].id, ctr)
-            solver.add(ctr)
+    def createDefuseConstraints(self):
+        '''All input and intermediate vars will appear at least once in the program'''
+        all_args = reduce(lambda a,b: a + b.args, self.lines, [])
+        for i in self.spec.get_param_productions():
+            ctr_input = reduce(lambda a,b: Or(a, b == i.id), all_args, False)
+            self.z3_solver.add(ctr_input)
 
-    def createFunctionConstraints(self, solver):
-        '''If a function occurs then set the function variable to 1 and 0 otherwise'''
-        assert len(self.nodes) == len(self.variables_fun)
-        for x in range(0, len(self.nodes)):
-            for p in self.spec.productions():
-                # FIXME: improve empty integration
-                if p.is_function() and str(p).find('Empty') == -1:
-                    ctr = Implies(
-                        self.variables[x] == p.id, self.variables_fun[x] == 1)
-                    solver.add(ctr)
-                else:
-                    ctr = Implies(
-                        self.variables[x] == p.id, self.variables_fun[x] == 0)
-                    solver.add(ctr)
-
-    def createLeafConstraints(self, solver):
-        for x in range(0, len(self.nodes)):
-            n = self.nodes[x]
-            if n.children is None:
-                ctr = self.variables[x] == self.leaf_productions[0].id
-                for y in range(1, len(self.leaf_productions)):
-                    ctr = Or(self.variables[x] ==
-                             self.leaf_productions[y].id, ctr)
-                solver.add(ctr)
-
-    def createChildrenConstraints(self, solver):
-        for x in range(0, len(self.nodes)):
-            n = self.nodes[x]
-            if n.children is not None:
-                for p in self.spec.productions():
-                    assert len(n.children) > 0
-                    for y in range(0, len(n.children)):
-                        ctr = None
-                        child_type = 'Empty'
-                        if p.is_function() and y < len(p.rhs):
-                            child_type = str(p.rhs[y])
-                        for t in self.spec.get_productions_with_lhs(child_type):
-                            if ctr is None:
-                                ctr = self.variables[n.children[y].id - 1] == t.id
-                            else:
-                                ctr = Or(
-                                    ctr, self.variables[n.children[y].id - 1] == t.id)
-                            ctr = Implies(self.variables[x] == p.id, ctr)
-                        solver.add(ctr)
+        if self.loc > 1:
+            for i in range(0, self.loc - 1):
+                def_var = self.lines[i].lhs
+                used_args = reduce(lambda a,b: a + b.args, self.lines[i+1:], [])
+                ctr_lhs = reduce(lambda a,b: Or(a, b == def_var), used_args, False)
+                self.z3_solver.add(ctr_lhs)
 
     def maxChildren(self) -> int:
         '''Finds the maximum number of children in the productions'''
@@ -141,28 +105,23 @@ class BidirectEnumerator(Enumerator):
                 max = len(p.rhs)
         return max
 
-    def buildKTree(self, children, depth):
-        '''Builds a K-tree that will contain the program'''
-        nodes = []
-        tree = AST()
-        root = ASTNode(1, 1)
-        nb = 1
-        tree.head = root
-        d = deque()
-        d.append(root)
-        nodes.append(root)
-        while len(d) != 0:
-            current = d.popleft()
-            current.children = []
-            for x in range(0, children):
-                nb += 1
-                c = ASTNode(nb, current.depth + 1)
-                nodes.append(c)
-                current.children.append(c)
-                if c.depth < depth:
-                    d.append(c)
-    
-        return tree, nodes
+    def buildKLines(self, children, loc, solver):
+        lines = []
+
+        for l in range(0,loc):
+            lhs_name = 'ret' + str(l)
+            lhs = Int(lhs_name)
+            opcode_name = 'opcode' + str(l)
+            opcode = Int(opcode_name)
+            args = []
+            for i in range(0, children):
+                arg_name = 'arg' + str(i) + '@' + str(l)
+                args.append(Int(arg_name))
+            st = Stmt(opcode, args, lhs)
+            lines.append(st)
+            self.z3_solver.add(lhs == (1000 + l))
+
+        return lines, None
 
     def __init__(self, spec, depth=None, loc=None):
         self.z3_solver = Solver()
@@ -180,16 +139,10 @@ class BidirectEnumerator(Enumerator):
                 'LOC cannot be non-positive: {}'.format(loc))
         self.loc = loc
         self.max_children = self.maxChildren()
-        self.tree, self.nodes = self.buildKTree(self.max_children, self.depth)
+        self.lines, self.nodes = self.buildKLines(self.max_children, self.loc, self.z3_solver)
         self.model = None
-        self.initLeafProductions()
-        self.createVariables(self.z3_solver)
-        self.createOutputConstraints(self.z3_solver)
-        self.createLocConstraints(self.z3_solver)
-        self.createInputConstraints(self.z3_solver)
-        self.createFunctionConstraints(self.z3_solver)
-        self.createLeafConstraints(self.z3_solver)
-        self.createChildrenConstraints(self.z3_solver)
+        self.createStmtConstraints()
+        self.createDefuseConstraints()
 
     def blockModel(self):
         assert(self.model is not None)
@@ -257,6 +210,7 @@ class BidirectEnumerator(Enumerator):
             res = self.z3_solver.check()
             if res == sat:
                 self.model = self.z3_solver.model()
+                print(self.model)
 
             if self.model is not None:
                 return self.buildProgram()
